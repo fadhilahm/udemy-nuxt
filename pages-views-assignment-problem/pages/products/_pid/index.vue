23ea9ce87aae65fc4f649574dcd10f97f85d8951
fadhilahm <fadhilahmetra@gmail.com>
2021-03-07 14:08:29 +0700

    Finished pages-views assignment

---
+++ pages-views-assignment-problem/pages/products/_pid/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>This is the product page for PID:</h1>
+        <h2>{{ $route.params.pid }}</h2>
+    </div>
+</template>
---
+++ pages-views-assignment-problem/pages/products/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <nuxt-link :to="`${$route.path}/1`">To Product 1</nuxt-link>
+        <nuxt-link :to="`${$route.path}/2`">To Product 2</nuxt-link>
+    </div>
+</template>
