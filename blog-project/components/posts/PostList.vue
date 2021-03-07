2ed08ae15f5adc0e01df82254eb40ce97e2044f5
fadhilahm <fadhilahmetra@gmail.com>
2021-03-07 23:09:12 +0700

    Finished part of the blog project

---
+++ blog-project/components/posts/PostList.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="post-list">
+    <post-preview
+      id="1"
+      thumbnail="https://cache.techmahindra.com/static/img/hi-tech-enterprise-smart-assistant.jpg"
+      title="Hello there! I'm the first post"
+      preview-text="Hello there! I'm the first preview text"
+    />
+    <post-preview
+      id="2"
+      thumbnail="https://cache.techmahindra.com/static/img/hi-tech-enterprise-smart-assistant.jpg"
+      title="Title me2"
+      preview-text="Preview me2"
+    />
+    <post-preview
+      id="3"
+      thumbnail="https://media.mehrnews.com/d/2018/10/28/4/2940617.jpg"
+      title="Third spot babyyyyyy"
+      preview-text="For the last hurrah..."
+    />
+  </section>
+</template>
+
+<script>
+import PostPreview from "@/components/posts/PostPreview";
+
+export default {
+  components: {
+    PostPreview
+  }
+};
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ blog-project/layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <the-header @sidenavToggle="displaySideNav = !displaySideNav"/>
+    <the-sidenav
+      :show="displaySideNav"
+      @close="displaySideNav = false"
+    />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/navigation/TheHeader';
+import TheSidenav from '@/components/navigation/TheSidenav';
+
+export default {
+  components: {
+    TheHeader,
+    TheSidenav
+  },
+  data() {
+    return {
+      displaySideNav: false
+    };
+  }
+}
+</script>
+
+<style>
+html {
+  font-family: 'Open sans', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
